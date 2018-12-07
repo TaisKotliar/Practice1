e61469d7c3de39e1687468c9db8561cc38626d49
Tais <kryos.eva@gmail.com>
2018-12-07 19:21:04 +0200

    Changed 10 method

--- src/Main.java
+++ src/Main.java
@@ -1,49 +1,78 @@
 import java.sql.SQLInvalidAuthorizationSpecException;
 
 public class Main {
-    static byte a;
-    static short a1 = 4;
-    static int a2 = 5;
-    static long a3 = 6;
-    static char a4 = '5';
-    static double a5 = 5.3;
-    static float a6 = 5.4f;
+    static byte a0;
+    static short a1;
+    static int a2;
+    static long a3;
+    static char a4;
+    static double a5;
+    static float a6;
 
     public static void main(String[] args) {
-
-        float b1 = 1f;
-        float b2 = 1f;
-        float b3 = 0x1;
-        float b4 = 0b1;
-        float b5 = 1.0e1f;
-
-        sumShort();
-        square();
-        simple();
-        number();
-        even();
-        abc(4, 2, 6);
-        range(23, 2);
-        bank(100,0.05, 2);
-        /*System.out.println(a);
+        System.out.println(a0);
         System.out.println(a1);
         System.out.println(a2);
         System.out.println(a3);
         System.out.println(a4);
         System.out.println(a5);
-        System.out.println(a6);*/
+        System.out.println(a6);
 
+       /* насколько я поняла задание, теперь создаем локальные переменные без инициалицации.
+       В этом случае вывести на экран уже не получится
+        byte a00;
+        short a11;
+        int a22;
+        long a33;
+        char a44;
+        double a55;
+        float a66;
 
+        System.out.println(a00);
+        System.out.println(a11);
+        System.out.println(a22);
+        System.out.println(a33);
+        System.out.println(a44);
+        System.out.println(a55);
+        System.out.println(a66);
+       */
+
+        float b0 = 1f;
+        float b1 = 1f;
+        float b2 = 0x1;
+        float b3 = 0b1;
+        float b4 = 1.0e1f;
+        float b5 = 01f;
+
+        System.out.println(b0);
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(b3);
+        System.out.println(b4);
+        System.out.println(b5);
+
+        sumShort();
+        square();
+        number();
+        even();
+        simple();
+        abc(4, 2, 6);
+        range(23, 2);
+        bank(1000, 0.36, 1);
     }
 
     public static void sumShort() {
         short c;
         c = 7 + 3;
-        System.out.println(c);
+        System.out.println("Short " + c);
         c = 6 + (short) 5.4;
-        System.out.println(c);
+        System.out.println("Short " + c);
         c = (short) 5.2f + 798;
-        System.out.println(c);
+        System.out.println("Short " + c);
+        c = (byte) 5 + 5;
+        System.out.println("Short " + c);
+        c = (short) 6.4f + (short) 4.2;
+        System.out.println("Short " + c);
     }
 
     public static void square() {
@@ -51,29 +80,31 @@ public class Main {
         int k2 = 4;
         int g = 5;
         String squarRes = (k1 * k1 + k2 * k2 == g * g) ? "yes" : "no";
-        //if?then:else
-        System.out.println(squarRes);
+        System.out.println("Is this triangles right-angled? " + squarRes);
     }
 
     public static void number() {
         int temp = 1;
         int sum = 0;
         while (temp <= 20) {
-
             sum += temp++;
-            //temp++;
         }
-        System.out.println(sum);
+        System.out.println("Total sum from 1 to 20 is " + sum);
     }
 
     public static void even() {
-        int sum1 = 0;
-        for (int temp1 = 0; temp1 <= 20; temp1 = temp1 + 2) {
-            sum1 += temp1;
+        int sumEven = 0;
+        int temp = 1;
+        while (temp <= 20) {
+            if ((temp % 2) == 0) {
+                sumEven += temp;
+            }
+            temp++;
         }
-        System.out.println(sum1);
+        System.out.println("Sum of even numbers is " + sumEven);
     }
-    public static void  simple(){
+
+    public static void simple() {
         int sum3 = 0;
         for (int temp3 = 0; temp3 <= 20; temp3++) {
             switch (temp3) {
@@ -89,33 +120,35 @@ public class Main {
                     break;
             }
         }
-        System.out.println("Simple sum " + sum3);
+        System.out.println("Sum of simple numbers " + sum3);
     }
+
     public static void abc(int a, int b, int c) {
         boolean resalt = false;
         boolean first1 = (a + b) == c;
         boolean first2 = (a + c) == b;
         boolean first3 = (c + b) == a;
-        if (first1 || first2 ||first3) {
+        if (first1 || first2 || first3) {
             resalt = true;
         }
-       System.out.println(resalt);
-
+        System.out.println("True of false? " + resalt);
     }
 
     public static void range(int a, int b) {
         if (a < b) {
             System.out.println("a must be > b");
         } else {
-            System.out.println((a + b) / 2);
+            System.out.println("Average value is " + (a + b) / 2);
         }
     }
 
-    public static void bank(int Sum, double P, int Time){
-        System.out.println("Sum Paymant: " + (Sum * P));
-        System.out.println("Month Payment: " + ((Sum + (Sum * P))/Time));
-        System.out.println("Month %: " + ((Sum * P)/Time));
+    public static void bank(int sum, double percent, int time) {
+        //Общая сумма с процентами
+        System.out.println("Sum Payment: " + (sum + (sum * (percent/12)*time)));
+        //Общая сумма выплаченых процентов
+        System.out.println("Total per cent: " + (sum * (percent/12)*time));
+        //Сумма выплаты в месяц вмечте с процентами
+        System.out.println("Month Payment: " + (((sum * ((percent/12)*time)) + sum) / time));
     }
-
 }
 
